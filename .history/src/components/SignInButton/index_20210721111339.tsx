9f88b62fcd3bb2e2862adc2ac952fbc572f87dee
alexandreifto2@gmail.com <alexandreifto2@gmail.com>
2021-07-21 11:23:35 -0300

    nocos

---
+++ .history/src/components/SignInButton/index_20210721111339.tsx
@@ -0,0 +1,9 @@
+import { FaGithub } from 'react-icons/fo'
+export function SignInButton(){
+    return(
+        <button>
+            <FaGithub/>
+            Sign Ins
+        </button>
+    );
+}
---
+++ .history/src/components/SignInButton/index_20210721112305.tsx
@@ -0,0 +1,9 @@
+import { FaGithub } from 'react-icons/fa'
+export function SignInButton(){
+    return(
+        <button>
+            <FaGithub/>
+            Sign Ins
+        </button>
+    );
+}
