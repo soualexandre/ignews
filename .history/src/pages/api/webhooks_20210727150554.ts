a0780f6c231bdb3463cdc303020fa88e18c8ca52
alexandreifto2@gmail.com <alexandreifto2@gmail.com>
2021-07-27 17:27:34 -0300

    new feature buy stripe

---
+++ .history/src/pages/api/webhooks_20210727150554.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {Readable} from 'stream'
+
+async function buffer(readable: Readable){
+    const chunks = [];
+
+    for await (const chunk of readable){
+        chunks.push(
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        );
+    }
+}
+
+export default ( req: NextApiRequest, res: NextApiResponse)=>{
+ 
+    
+
+    res.status(200).json({ok: true})
+}
---
+++ .history/src/pages/api/webhooks_20210727150626.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {Readable} from 'stream'
+
+async function buffer(readable: Readable){
+    const chunks = [];
+
+    for await (const chunk of readable){
+        chunks.push(
+            typeof chunk === "string" ? Buffer.from(chunk) : chunk
+        );
+    }
+    return Buffer.concat(chunks); 
+}
+
+export default ( req: NextApiRequest, res: NextApiResponse)=>{
+ 
+    
+
+    res.status(200).json({ok: true})
+}
