46f0eefce9c1d669cef665f856d6159abd1a62d8
soualexandre <alexandreifto2@gmail.com>
2021-07-30 19:06:30 -0300

    finish first module to captcher three

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,19 @@
 import { SignInButton } from '../SignInButton'
-import {useRouter} from 'next/router'
 import styles from './styles.module.scss'
-import Link from 'next/link'
+import { ActiveLink } from '../ActiveLink'
 export function Header() {
-    const { asPath } = useRouter()
-    console.log(asPath)
+   
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <Link href="/" >
-                        <a className={asPath === '/' ? styles.active : ''}>Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
-                        <a className={asPath === '/posts' ? styles.active : ''}>Posts</a>
-                    </Link>
+                    <ActiveLink activeClassName={styles.active} href="/" >
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink activeClassName={styles.active}  href="/posts" prefetch>
+                        <a>Posts</a>
+                    </ActiveLink>
                 </nav>
                 <SignInButton />
             </div>
